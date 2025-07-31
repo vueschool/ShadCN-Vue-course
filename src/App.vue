b594414771c91ea7c4166603d900cf9033089724
Daniel Kelly <me@danielkelly.io>
2025-07-31 16:14:32 -0500

    2-install-shadcn-vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div>
+    <Button>Click me</Button>
+  </div>
 </template>
-
-<style scoped></style>
